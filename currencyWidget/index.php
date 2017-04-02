cc8f3292911a60295e6075ea481022161207a4f2
MaxTul <maximti@ac.sce.ac.il>
2017-04-02 10:44:11 +0300

    widget primary version

---
+++ currencyWidget/index.php
@@ -0,0 +1,48 @@
+<?php header("Content-Type: text/html; charset=utf-8");?>
+<DOCTYPE html>
+<html>
+    <head>
+    <title>Currency Widget</title>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script src="currentScripts.js"></script>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<meta charset="utf-8" />
+	<meta http-equiv="refresh" content="43200">
+    </head>
+	<body>
+	
+    <div id="container">
+  
+  <select id="curr_index">
+
+                    <option value="0">Dollar USA</option>
+                    <option value="1">Pound Great Britain</option>
+                    <option value="2">Yen Japan</option>
+                    <option value="3">Euro</option>
+                    <option value="4">Dollar Australia</option>
+		            <option value="5">Dollar Canada</option>
+		            <option value="6">Krone Denmark</option>
+		            <option value="7">Krone Norway</option>
+		            <option value="8">Rand South Africa</option>
+		            <option value="9">Krona Sweden</option>
+		            <option value="10">Franc Switzerland</option>
+		            <option value="11">Dinar Jordan</option>
+		            <option value="12">Pound Lebanon</option>
+		            <option value="13">Pound Egypt</option>
+  
+    </select>
+  
+    <div id="result">
+    </div>	
+
+	
+	<br> 
+    <input type="button" value="Reset widget to default" onClick="window.location.reload()"> 
+  
+  
+
+  </body> 
+  
+      
+  
+</html>
